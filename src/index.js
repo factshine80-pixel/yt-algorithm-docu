4a35250f55e13f51faef798d8f19009e85190778
factshine80-pixel <factshine80@gmail.com>
2026-01-30 09:08:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
-import {Composition} from 'remotion';
-import {MainVideo} from './main';
+import {registerRoot} from 'remotion';
+import {RemotionRoot} from './MainVideo';
 
-export const RemotionRoot = () => {
-  return (
-    <>
-      <Composition
-        id="MainVideo"
-        component={MainVideo}
-        durationInFrames={300}
-        fps={30}
-        width={1920}
-        height={1080}
-      />
-    </>
-  );
-};
+registerRoot(RemotionRoot);
